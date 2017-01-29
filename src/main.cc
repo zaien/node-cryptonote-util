1da22ecaf42b3e84630d48fd61309255747556bc
Alexander Blair <alex@snipanet.com>
2017-01-28 21:27:44 -0800

    Update to rearrange more data.

--- src/main.cc
+++ src/main.cc
@@ -349,6 +349,7 @@ void address_decode_integrated(const Nan::FunctionCallbackInfo<v8::Value>& info)
 
 
     integrated_address iadr;
+    account_public_address adr;
     adr = iadr.adr;
     if (!::serialization::parse_binary(data, iadr) || !crypto::check_key(adr.m_spend_public_key) || !crypto::check_key(adr.m_view_public_key))
     {
